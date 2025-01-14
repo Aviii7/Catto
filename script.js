67c954d69e537e52cfbaec36bfe8b05020ba0520
Aviii7 <jacdaguro22@src.pshs.edu.ph>
2025-01-14 14:21:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.write('Hello, I am from a new file!');
+
+const gravitationalConstant = 6.67 * 10e-11;
+var mass1 = 5;
+var mass2 = 7;
+var distance = 100;
+let gravitationalForce = (gravitationalConstant * mass1 * mass2)/(distance * distance);
+
