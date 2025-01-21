ab15fe3bf32fdca8ec73e53b23f66adbc4dec883
Aviii7 <jacdaguro22@src.pshs.edu.ph>
2025-01-21 19:58:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+let age = 14;
+let maxAge = 120;
+let snackPerDay = 1;
+let totalAmount = ((maxAge - age) * 365.25) * snackPerDay
 
+document.write('You will be able to eat' + ' ' + totalAmount + ' ' + 'bowls of champorado before you hit' + ' ' + maxAge + ' ' + 'years old.')
