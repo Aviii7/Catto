80a23e11d16834c2d27e289fc9853a86d52a216b
Aviii7 <jacdaguro22@src.pshs.edu.ph>
2025-01-21 19:50:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 
-const gravitationalConstant = 6.67 * 10e-11;
-var mass1 = 5;
-var mass2 = 7;
-var distance = 100;
-let gravitationalForce = (gravitationalConstant * mass1 * mass2)/(distance * distance);
-
-document.write(gravitationalForce);
-
